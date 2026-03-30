1c14e54401cb252aa9c3221a557505fbca1582bc
crybaby05 <benthiam750@gmail.com>
2026-03-30 02:33:07 +0000

    test

---
+++ model/other/recup.php
@@ -0,0 +1,14 @@
+<?php
+//fonction qui me permettent de transformer les données json en donée php exploitable
+function jsonToArray(){
+    $path = __DIR__ . '/../../data/users.json';
+    $json = file_get_contents($path);
+    return json_decode($json, true);
+}
+//fonction qui me permettent de transformer des données php en données completment exploitable en json
+function arrayToJson($array){
+    $path = __DIR__ . '/../../data/users.json';
+    $tab = json_encode($array, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE);
+    return file_put_contents($path,$tab);
+}
+?>
